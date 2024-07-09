73568ee7d269e0e457319c4cc13716d8e90075cf
kwamekert <kwamekert@gmail.com>
2024-07-10 00:56:13 +0200

    added eureka client

--- src/main/java/com/seso/audit_trail/AuditTrailApplication.java
+++ src/main/java/com/seso/audit_trail/AuditTrailApplication.java
@@ -2,10 +2,12 @@ package com.seso.audit_trail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableDiscoveryClient
 public class AuditTrailApplication {
 
 	public static void main(String[] args) {
